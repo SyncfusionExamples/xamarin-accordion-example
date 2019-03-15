33640cd218a409b9e88fe6e68e61b3e2b43e6fb4
Nijamudeenm M <nijamudeensulaiman@syncfusion.com>
2019-03-15 11:02:59 +0530

    Uploading Getting started sample for accordion control Xamarin

---
+++ AccordionGettingStarted/GettingStarted/GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.XForms.Accordion;
+using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+	public partial class MainPage : ContentPage
+	{
+        SfAccordion accordion;
+		public MainPage()
+		{
+			InitializeComponent();
+            accordion = new SfAccordion();
+		}
+	}
+}
